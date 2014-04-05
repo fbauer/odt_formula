fc1c443c1ad200afde88a655e9e32c302550a400
Florian Bauer <fbauer.devel@gmail.com>
2014-04-05 14:36:54 +0200

    Remove outdated emacs settings

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((python-mode
-  (pytest . "\.env/bin/py\.test")
-  (py-python-command . "\.env/bin/python")))
-
